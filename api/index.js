89c68f7325e49bb4c4a50abeb708d3041fa33664
JCPrime01 <vanzokiller@gmail.com>
2026-07-10 23:09:11 -0300

    chore: shareCode BUAAEARW9EI -> VZ6229LQ57N (via painel)

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=30010'
 
-    const shareCode = '&shareCode=BUAAEARW9EI'
+    const shareCode = '&shareCode=VZ6229LQ57N'
     
     const afp = '&afp3=bott'
     
