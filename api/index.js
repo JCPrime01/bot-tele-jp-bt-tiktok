40ea789d352b23582a1943f448938ddd577520cc
JCPrime01 <vanzokiller@gmail.com>
2026-07-13 22:36:12 -0300

    chore: shareCode 3AGPONWCZJK -> 12NHQPQOEPX (via painel)

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=30010'
 
-    const shareCode = '&shareCode=3AGPONWCZJK'
+    const shareCode = '&shareCode=12NHQPQOEPX'
     
     const afp = '&afp3=bott'
     
