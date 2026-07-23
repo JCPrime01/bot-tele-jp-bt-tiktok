721107e58af74b3e6f052d86ed7cf24e27b055d9
JCPrime01 <vanzokiller@gmail.com>
2026-07-22 23:15:53 -0300

    chore: shareCode 6L9RUAEEPJB -> EETW589DLXS (via painel)

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=30010'
 
-    const shareCode = '&shareCode=6L9RUAEEPJB'
+    const shareCode = '&shareCode=EETW589DLXS'
     
     const afp = '&afp3=bott'
     
