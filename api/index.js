6eed6124b2ba2406032dabf3d4a296a7169d2e8f
JCPrime01 <vanzokiller@gmail.com>
2026-07-29 07:02:47 -0300

    chore: shareCode L6U1FVVRHVV -> O6QYDL9YS2O (via painel)

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=30010'
 
-    const shareCode = '&shareCode=L6U1FVVRHVV'
+    const shareCode = '&shareCode=O6QYDL9YS2O'
     
     const afp = '&afp3=bott'
     
